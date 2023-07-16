c5a85cba5d9466fa0db35b50e8b1ffba7e433a82
Tudor <tudormatei010@gmail.com>
2023-07-16 10:03:53 +0300

    code refactor

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,32 +1,23 @@
-import logo from "../assets/audio-insight-logo.png";
-import icon from "../assets/audio-insight-icon.png";
-import microphone from "../assets/audio-insight-microphone.png";
-import stop from "../assets/audio-insight-stop.png";
+import logo from "../assets/botgen-logo-dark.png";
 import lightMode from "../assets/light-mode.png";
 import darkMode from "../assets/dark-mode.png";
-import logoLight from "../assets/audio-insight-logo-light.png";
+import logoLight from "../assets/botgen-logo-light.png";
 import hamburgerLight from "../assets/hamburger-light.png";
 import hamburgerDark from "../assets/hamburger-dark.png";
 import tickLight from "../assets/tick-light.png";
 import tickDark from "../assets/tick-dark.png";
 import laptop from "../assets/laptop.png";
-import automation from "../assets/automation.jpg";
-import automationIcon from "../assets/automation.png";
-import microphoneIcon from "../assets/microphone.png";
+import automation from "../assets/customisation.jpg";
+import automationIcon from "../assets/gear.png";
 import cloudIcon from "../assets/cloud.png";
 import supportIcon from "../assets/support.png";
 import fastIcon from "../assets/fast.png";
 import call from "../assets/call.png";
 import linkedIn from "../assets/linkedin.png";
-import upload from "../assets/upload.jpg";
-import recording from "../assets/recording.png";
 import implementation from "../assets/implementation.jpg";
 
 export default {
   logo,
-  icon,
-  microphone,
-  stop,
   lightMode,
   darkMode,
   logoLight,
@@ -37,11 +28,8 @@ export default {
   laptop,
   automation,
   automationIcon,
-  microphoneIcon,
   cloudIcon,
   linkedIn,
-  upload,
-  recording,
   fastIcon,
   supportIcon,
   call,
